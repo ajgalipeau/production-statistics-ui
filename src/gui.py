1900be34d80350d0a94b78b95854dccfb28272ef
Aaron Galipeau <34598583+ajgalipeau@users.noreply.github.com>
2024-01-18 16:14:38 -0800

    Added calculator functions

--- src/gui.py
+++ src/gui.py
@@ -1,7 +1,11 @@
 # Import modules
 from PyQt5.QtWidgets import QMainWindow, QAction, QMenu
 import math
+import matplotlib.pyplot as plt
+from math import sqrt, comb, exp, factorial, ceil
 from collections import Counter
+from scipy.stats import norm
+
 
 class StatsCalculatorGUI(QMainWindow):
     def __init__(self):
@@ -13,25 +17,31 @@ class StatsCalculatorGUI(QMainWindow):
         pass
 
     # Define methods for each calculator input
-    # Example:
+    
+    # Combinations
     def combinations(n, r):
         return math.comb(n, r)
 
+    # Permutations
     def permutations(n, r):
         return math.perm(n, r)
 
+    # Variance
     def variance(data):
         mean = sum(data) / len(data)
         return sum((x - mean) ** 2 for x in data) / len(data)
 
+    # Standard Deviation
     def standard_deviation(data):
         mean = sum(data) / len(data)
         variance = sum((x - mean) ** 2 for x in data) / len(data)
         return math.sqrt(variance)
 
+    # Mean
     def mean(data):
         return sum(data) / len(data)
 
+    # Median
     def median(data):
         data_sorted = sorted(data)
         n = len(data_sorted)
@@ -42,7 +52,103 @@ class StatsCalculatorGUI(QMainWindow):
         else:
             return data_sorted[mid]
 
+    # Mode
     def mode(data):
         count = Counter(data)
         max_count = max(count.values())
         return [k for k, v in count.items() if v == max_count]
+
+    # Probability
+    def probability(success_outcomes, total_outcomes):
+        return success_outcomes / total_outcomes
+
+    # Z-score
+    def z_score(data_point, mean, standard_deviation):
+        return (data_point - mean) / standard_deviation
+
+    # T-test
+    def t_test(mean1, mean2, std1, std2, n1, n2):
+        pooled_std = sqrt(((n1 - 1) * std1**2 + (n2 - 1) * std2**2) / (n1 + n2 - 2))
+        t_score = (mean1 - mean2) / (pooled_std * sqrt(1/n1 + 1/n2))
+        return t_score
+
+    # Chi-square
+    def chi_square_test(observed, expected):
+        chi_square = sum((o - e)**2 / e for o, e in zip(observed, expected))
+        return chi_square
+
+    # Linear Regression
+    def linear_regression(x, y):
+        n = len(x)
+        xy = sum(xi * yi for xi, yi in zip(x, y))
+        sum_x = sum(x)
+        sum_y = sum(y)
+        square_sum_x = sum(xi**2 for xi in x)
+
+        slope = ((n * xy) - (sum_x * sum_y)) / ((n * square_sum_x) - (sum_x**2))
+        intercept = (sum_y - (slope * sum_x)) / n
+
+        return slope, intercept
+
+    # ANOVA (Analysis of Variance)
+    def anova(*groups):
+        # Number of groups and total number of observations
+        k = len(groups)
+        n = sum(len(group) for group in groups)
+
+        # Overall mean
+        overall_mean = sum(sum(group) for group in groups) / n
+
+        # Between-group variability
+        ss_between = sum(len(group) * (sum(group) / len(group) - overall_mean) ** 2 for group in groups)
+
+        # Within-group variability
+        ss_within = sum(sum((item - sum(group) / len(group)) ** 2 for item in group) for group in groups)
+
+        # Mean sum of squares
+        ms_between = ss_between / (k - 1)
+        ms_within = ss_within / (n - k)
+
+        # F-ratio
+        f_ratio = ms_between / ms_within
+        return f_ratio
+
+    from scipy.stats import norm
+
+    # Confidence interval
+    def confidence_interval(mean, std, n, confidence_level):
+        z = norm.ppf(1 - (1 - confidence_level) / 2)
+        margin_of_error = z * (std / (n ** 0.5))
+        return (mean - margin_of_error, mean + margin_of_error)
+    
+    # Binomial Distribution
+    def binomial_probability(n, k, p):
+    return comb(n, k) * (p ** k) * ((1 - p) ** (n - k))
+
+    # Poisson Distribution
+    def poisson_probability(lambda_, k):
+        return (lambda_ ** k) * exp(-lambda_) / factorial(k)
+
+    # Normal Distribution
+    def normal_probability(mean, std, x):
+        return norm.cdf(x, mean, std)
+
+    # Sample size
+    def sample_size(margin_of_error, confidence_level, p_hat):
+        z = norm.ppf(1 - (1 - confidence_level) / 2)
+        return ceil((z**2 * p_hat * (1 - p_hat)) / margin_of_error**2)
+
+    # Create Histogram
+    def create_histogram(data, bins=None):
+        plt.hist(data, bins=bins)
+        plt.xlabel('Data')
+        plt.ylabel('Frequency')
+        plt.title('Histogram')
+        plt.show()
+
+    # Create Box Plot
+    def create_box_plot(data):
+        plt.boxplot(data)
+        plt.ylabel('Data')
+        plt.title('Box-and-Whisker Plot')
+        plt.show()
